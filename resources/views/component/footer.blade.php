9ec5d2bf709f868fb75b981c9d363f8a13243a39
Patrick Jonathan <pj141299@gmail.com>
2023-06-16 14:42:26 +0700

    Screenshot

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -2,10 +2,10 @@
     <p class="leftF">© BiSearch, 2023</p>
     <p class="rightF">
         Find us at
-        <a href="">
+        <a href="https://www.instagram.com/bisearchweb/?igshid=MmJiY2I4NDBkZg%3D%3D" target="blank">
             <i class="fa-brands fa-2xl fa-instagram logo insta"></i>
         </a>
-        <a href="">
+        <a href="https://twitter.com/bisearchweb?s=11&t=d7PA6TwJs2yxkK5n28xmNw" target="blank">
             <i class="fa-brands fa-2xl fa-square-twitter logo"></i>
         </a>
     </p>
